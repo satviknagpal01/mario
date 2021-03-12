ca4d1da9e8a702aa1b9bee084ae779d0cfa0303c
Satvik Nagpal <satviknagpal2401@gmail.com>
2021-03-12 19:03:45 +0530

    new

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    public float speed = -4f;
+    Vector2 velocity;
+    private new Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
     }
 }
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f;
     public float jumpspeed = 5f;
+    private bool Isgrounded = true;
     Vector2 velocity;
     private new Rigidbody2D rigidbody2D;
     private SpriteRenderer spriterenderer;
@@ -21,13 +23,29 @@ public class player : MonoBehaviour
     {
         var h = Input.GetAxis("Horizontal");
         rigidbody2D.velocity = new Vector2(h * speed, rigidbody2D.velocity.y);
-        if(h<0)
+        if(h!=0)
         {
             spriterenderer.flipX = h<0;
         }
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && Isgrounded)
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpspeed);
         }
+
     }
-}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            Isgrounded = true;
+
+        if (collision.gameObject.tag == "ground")
+            Debug.Log("Collision with plant");
+        if (collision.gameObject.tag == "Enemy")
+            SceneManager.LoadScene(1);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            Isgrounded = false;
+    }
+}   
