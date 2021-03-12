53745f20347797d3e51551bb89e2f208012f090f
Satvik Nagpal <satviknagpal2401@gmail.com>
2021-03-12 10:12:09 +0530

    new

--- Assets/scripts/camerafollow.cs
+++ Assets/scripts/camerafollow.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class camerafollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
-    void Update()
+  //  public float offsetX;
+    private void FixedUpdate()
     {
-        
+        float playerXPos = target.position.x;
+        float cameraXPos = Mathf.Clamp(playerXPos, 0.2f, 100f);
+        transform.position = new Vector3(cameraXPos, 0f, -10f);
     }
 }
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpspeed = 5f;
+    Vector2 velocity;
+    private new Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriterenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(h * speed, rigidbody2D.velocity.y);
+        if(h<0)
+        {
+            spriterenderer.flipX = h<0;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpspeed);
+        }
+    }
+}
